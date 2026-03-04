6ee8280cf96ed1fa38e64509af180aec9995ce62
Arifin Rahman Niloy <arniloy10official@gmail.com>
2026-03-04 23:23:38 +0600

    Create data/questions.js file containing mental health assessment questions

---
+++ data/questions.js
@@ -0,0 +1,41 @@
+# Mental Health Assessment Questions
+
+## Section 1: General Mental Health
+1. How often have you felt down, depressed, or hopeless?
+2. How often have you felt little interest or pleasure in doing things?
+3. How often have you had trouble sleeping, including falling asleep, staying asleep, or sleeping too much?
+
+## Section 2: Anxiety
+4. How often have you felt nervous, anxious, or on edge?
+5. How often have you been unable to stop or control worrying?
+6. How often have you worried too much about different things?
+
+## Section 3: Stress
+7. How often have you found it difficult to relax?
+8. How often have you felt overwhelmed by your problems?
+9. How often have you felt that you were unable to cope with all the stressors in your life?
+
+## Section 4: Relationships
+10. How often have you felt isolated or alone?
+11. How often have your relationships caused you distress?
+12. How often have you felt supported by your friends and family?
+
+## Section 5: Sleep
+13. How often have you experienced difficulties with sleep?
+14. How often do you feel tired or fatigued during the day?
+15. How often have you used medication to help you sleep?
+
+## Section 6: Substance Use
+16. How often have you used alcohol or drugs to cope with your feelings?
+17. How often have you felt that you need to cut down on your drinking or drug use?
+18. How often have you felt guilt or remorse after using substances?
+
+## Section 7: Self-Esteem
+19. How often have you felt worthless or a failure?
+20. How often do you doubt your abilities?
+21. How often have you been able to celebrate your successes?
+
+## Section 8: Safety and Support
+22. How often have you felt safe in your environment?
+23. How often have you sought help when you needed it?
+24. How often have you thought about harming yourself or others?
